0883671399e2e1cd53e7fcec23e0b3577d36546c
21Coochie <brghtmalone@gmail.com>
2025-07-13 03:54:34 +0000

    feat: Enhance App.vue with lazy loading for footer and loading state management

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { onMounted, watch } from 'vue'
+import { defineAsyncComponent, onMounted, ref, watch } from 'vue'
 import { useAuthStore } from './stores/AuthStore'
 import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 import { useBookStore } from './stores/BookStore'
+
+const isLoaded = ref(false)
 
 const router = useRouter()
 const bookStore = useBookStore()
@@ -12,6 +14,7 @@ const { isLoggedIn, user } = storeToRefs(authStore)
 
 onMounted(async () => {
   if (!authStore.userChecked) await authStore.getCurrentUser()
+  isLoaded.value = true
 })
 
 watch([isLoggedIn, user], async ([newIsLoggedIn, newUser], [oldIsLoggedIn, oldUser]) => {
@@ -30,14 +33,18 @@ watch([isLoggedIn, user], async ([newIsLoggedIn, newUser], [oldIsLoggedIn, oldUs
     await bookStore.refreshBooks()
   }
 })
+
+const FooterComponentLazy = defineAsyncComponent({
+  loader: () => import('@/components/app/Footer.vue'),
+})
 </script>
 
 <template>
   <UApp>
     <div>
       <Navbar />
-      <RouterView />
-      <Footer />
+      <RouterView v-if="isLoaded" />
+      <FooterComponentLazy v-if="isLoaded" />
     </div>
   </UApp>
 </template>
